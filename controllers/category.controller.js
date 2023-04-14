22ac7212eb5c4e679c67ffc46ccccd2a1bb02f8d
danialarrifin <danialrrifin11@gmail.com>
2023-04-15 00:15:49 +0800

    updated category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -18,26 +18,10 @@ const getCategory = async (req, res) => {
     return res.json(category);
 }
 const createCategory = async (req, res) => {
-     //     // 1. create address
-    const address = await Address.create({
-        address: req.body.address,
-        state: req.body.state,
-        postcode: req.body.postcode,
-        city: req.body.city,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude,
-    });
-    //     // 2. create workshop
-    const workshop = await Workshop.create({
-        name: req.body.name,
-        contact_num: req.body.contact_no,
-        address_id: address.id,
-    });
-
-    // 3. create category
+ // 3. create category
     const category = await Category.create({
         name: req.body.name,
-        workshop_id: workshop.id,
+        workshop_id: req.body.workshop_id,
     });
     return res.json(category);
 };
@@ -45,7 +29,7 @@ const createCategory = async (req, res) => {
 const updateCategory= async (req, res) => {
     const category = await Category.update({
         name: req.body.name,
-        workshop_id: workshop.id,
+        workshop_id: req.body.workshop.id,
     }, {
         where: {
             id: req.query.categoryId
