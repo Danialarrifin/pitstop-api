57b63daf3e04e5257f543768dbcee0aeddc92cb9
danialarrifin <danialrrifin11@gmail.com>
2023-04-17 23:36:48 +0800

    userId to AppointmentId

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -1,9 +1,10 @@
-const { Appointment } = require('../models');
+const { Op } = require('sequelize');
+const { Appointment, Time_slot } = require('../models');
 
 const getAllAppointment = async (req, res) => {
     let appointments;
-    if (req.query.userId)
-        appointments = await Appointment.findByPk(req.query.appointmentId)
+    if (req.query.appointmentId)
+        appointments = await Appointment.findByPk(req.query.appoinmentId)
     else
         appointments = await Appointment.findAll({});
 
@@ -18,16 +19,16 @@ const getAppointment = async (req, res) => {
     return res.json(appointment);
 }
 const createAppointment = async (req, res) => {
-    // 3. create appointment
-    const appointment = await Appointment.create({
-        date: req.body.date,
-        time_slot_id: req.body.time_slot_id,
-        vehicle_id: req.body.vehicle_id,
-        service_id: req.body.service_id,
-        status: req.body.status,
-
-    });
-    return res.json(appointment);
+    
+        const appointment = await Appointment.create({
+            date: req.body.date,
+            time_slot_id: req.body.time_slot_id,
+            vehicle_id: req.body.vehicle_id,
+            service_id: req.body.service_id,
+            status: req.body.status,
+        });
+        return res.json(appointment);
+    
 };
 
 const updateAppointment = async (req, res) => {
@@ -45,7 +46,7 @@ const updateAppointment = async (req, res) => {
     return res.json({ message: 'appointment successfully updated' });
 };
 
-const deleteAppointment= async (req, res) => {
+const deleteAppointment = async (req, res) => {
     const appointment = await Appointment.destroy({
         where: {
             id: req.query.appointmentId
