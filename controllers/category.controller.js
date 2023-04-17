9b72d8933522de3cb9580fa7b5e174324d6356e6
danialarrifin <danialrrifin11@gmail.com>
2023-04-17 23:55:38 +0800

    error cheking done

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,32 +1,44 @@
-const { Category, Workshop, Address} = require('../models');
+const { Category, Workshop, Address } = require('../models');
 
 const getAllCategory = async (req, res) => {
     let categories;
-    if(req.query.userId)
+    if (req.query.categoryId)
         categories = await Category.findByPk(req.query.categoryId)
     else
         categories = await Category.findAll({});
 
-    if(categories)
+    if (categories)
         return res.json(categories);
     else
         return res.status(400).json({ message: 'categories not found' })
 }
 
 const getCategory = async (req, res) => {
-    const category= await Category.findByPk(req.query.categoryId)
+    const category = await Category.findByPk(req.query.categoryId)
     return res.json(category);
 }
 const createCategory = async (req, res) => {
- // 3. create category
-    const category = await Category.create({
-        name: req.body.name,
-        workshop_id: req.body.workshop_id,
+    const { count, rows } = await Category.findAndCountAll({
+        where: {
+            name: req.body.name,
+            workshop_id: req.body.workshop_id,
+        }
     });
-    return res.json(category);
+    console.log("count", count)
+    if (count > 0) {
+        // duplicate found, return error
+        return res.status(400).json({ message: 'name already exists' })
+    } else {
+
+        const category = await Category.create({
+            name: req.body.name,
+            workshop_id: req.body.workshop_id,
+        });
+        return res.json(category);
+    }
 };
 
-const updateCategory= async (req, res) => {
+const updateCategory = async (req, res) => {
     const category = await Category.update({
         name: req.body.name,
         workshop_id: req.body.workshop.id,
@@ -38,7 +50,7 @@ const updateCategory= async (req, res) => {
     return res.json({ message: 'category successfully updated' });
 };
 
-const deleteCategory= async (req, res) => {
+const deleteCategory = async (req, res) => {
     const category = await Category.destroy({
         where: {
             id: req.query.categoryId
