d0913cec38e0388c49c581ebb5ca56ca12307b35
danialarrifin <danialrrifin11@gmail.com>
2023-05-16 01:43:31 +0800

    delete and update user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,12 +2,12 @@ const { User } = require('../models');
 
 const getAllUser = async (req, res) => {
     let users;
-    if(req.query.userId)
+    if (req.query.userId)
         users = await User.findByPk(req.query.userId)
     else
         users = await User.findAll({});
 
-    if(users)
+    if (users)
         return res.json(users);
     else
         return res.status(400).json({ message: 'user not found' })
@@ -18,7 +18,53 @@ const getUser = async (req, res) => {
     return res.json(user);
 }
 
+const createUser = async (req, res) => {
+    const { count, rows } = await Vehicle.findAndCountAll({
+        where: {
+            email: req.body.email,
+        }
+    });
+    console.log("count", count)
+    if (count > 0) {
+        // duplicate found, return error
+        return res.status(400).json({ message: 'email plate exists' })
+    } else {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        return res.json(user);
+    }
+
+};
+
+const updateUser = async (req, res) => {
+    const user = await User.update({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }, {
+        where: {
+            id: req.query.userId
+        }
+    });
+    return res.json({ message: 'user successfully updated' });
+};
+
+const deleteUser = async (req, res) => {
+    const user = await User.destroy({
+        where: {
+            id: req.query.userId
+        }
+    });
+    return res.json({ message: 'user successfully deleted' });
+};
+
+
 module.exports = {
     getAllUser,
     getUser,
+    updateUser,
+    deleteUser,
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,9 @@ const { userController } = require('../controllers');
 const ErrorHandler = require('../middleware/error.middleware');
 const AuthGuard = require('../middleware/auth.middleware');
 const validate = require('../utils/validator.util');
+const { userValidation } = require('../validations');
 
 router.get('/', AuthGuard, ErrorHandler(userController.getAllUser));
-
+router.post('/update', AuthGuard, validate(userValidation.updateUser), ErrorHandler(userController.updateuser));
+router.get('/delete', AuthGuard, ErrorHandler(userController.deleteUser));
 module.exports = router;
