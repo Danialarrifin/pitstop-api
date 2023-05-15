b347b8cc0ace142301ef60ec3229d492774d7f07
danialarrifin <danialrrifin11@gmail.com>
2023-05-16 01:43:13 +0800

    package json

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', indexRouter);
+
 
 module.exports = app;
