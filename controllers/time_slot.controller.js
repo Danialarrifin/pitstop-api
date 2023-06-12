e885d07b3fb8eb2287c3ced6012eb024a2ab53c1
danialarrifin <danialrrifin11@gmail.com>
2023-06-12 20:25:02 +0800

    by default a bit issue try to selectAll

--- controllers/time_slot.controller.js
+++ controllers/time_slot.controller.js
@@ -1,14 +1,20 @@
+const sequelize = require('../config/connection');
 const { Time_slot } = require('../models');
 
 const getAllTime_slot = async (req, res) => {
+    console.log('req', req.query);
     let time_slots;
-    if (req.query.time_slotId)
-        time_slots = await Time_slot.findByPk(req.query.time_slotId)
+    if (req.query.workshopId)
+        time_slots = await sequelize.query(`SELECT * FROM time_slots WHERE workshop_id = ${req.query.workshopId}`)
     else
-        time_slots = await Time_slot.findAll({});
+        time_slots = await sequelize.query(`SELECT * FROM time_slots`)
 
-    if (time_slots)
-        return res.json(time_slots);
+    if (time_slots) {
+        if (time_slots.length > 0)
+            return res.json(time_slots[0]);
+        else
+            return res.json(time_slots);
+    }
     else
         return res.status(400).json({ message: 'time_slots not found' })
 }
@@ -26,7 +32,7 @@ const createTime_slot = async (req, res) => {
     });
     console.log("count", count)
 
-    if(count > 0){
+    if (count > 0) {
         // duplicate found, return error
         return res.status(400).json({ message: 'time slot already exists' })
     } else {
