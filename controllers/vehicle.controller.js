ebd81baa1c6700fd074298ac91f5e4328082e2de
danialarrifin <danialrrifin11@gmail.com>
2023-06-19 21:59:45 +0800

    send by user id

--- controllers/vehicle.controller.js
+++ controllers/vehicle.controller.js
@@ -1,15 +1,22 @@
 const { Vehicle } = require('../models');
+const sequelize = require('../config/connection');
 
 const getAllVehicle = async (req, res) => {
     let vehicles;
     if (req.query.vehicleId)
         vehicles = await Vehicle.findByPk(req.query.vehicleId)
+    else if (req.query.userId) 
+        vehicles = await sequelize.query(`SELECT * FROM vehicles WHERE user_id = ${req.query.userId}`);
     else
-        vehicles = await Vehicle.findAll({});
+    vehicles = await sequelize.query(`SELECT * FROM vehicles`);
 
-    if (vehicles)
-        return res.json(vehicles);
-    else
+        if (vehicles) {
+            if (vehicles.length > 0)
+                return res.json(vehicles[0]);
+            else
+                return res.json(vehicles);
+        }
+        els
         return res.status(400).json({ message: 'vehicle not found' })
 };
 
@@ -20,9 +27,9 @@ const createVehicle = async (req, res) => {
         }
     });
     console.log("count", count)
-    if(count>0) {
-  // duplicate found, return error
-  return res.status(400).json({ message: 'num plate exists' })
+    if (count > 0) {
+        // duplicate found, return error
+        return res.status(400).json({ message: 'num plate exists' })
     } else {
         const vehicle = await Vehicle.create({
             user_id: req.body.user_id,
@@ -33,7 +40,7 @@ const createVehicle = async (req, res) => {
         });
         return res.json(vehicle);
     }
-    
+
 };
 
 const updateVehicle = async (req, res) => {
